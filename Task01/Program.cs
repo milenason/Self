ff2b28acb2d5e0a68ecf0cbf6683fd0a6086ce8e
Milena Son <son-milena@mail.ru>
2023-09-12 21:56:29 +0300

    Task01 Сон

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string surname, name, patronymic;
+        name = Console.ReadLine();
+        surname = Console.ReadLine();
+        patronymic = Console.ReadLine();
+        Console.WriteLine($"Фамилия:    {surname}");
+        Console.WriteLine($"Имя:        {name}");
+        Console.WriteLine($"Отчество:   {patronymic}");
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string name, surname, patronymic;
+        name = Console.ReadLine();
+        surname = Console.ReadLine();
+        patronymic = Console.ReadLine();
+        Console.WriteLine($"Имя:        {name}");
+        Console.WriteLine($"Фамилия:    {surname}");
+        Console.WriteLine($"Отчество:   {patronymic}");
+    }
+}
